5773e95e29feece66825db225805c4d39cd0846c
Thanasis <makris_th@hotmail.com>
2025-10-18 11:26:51 +0300

    service layer [handles low level http] reusable

---
+++ src/app/core/services/base.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_BASE } from '../constants/api.constant';
+import { catchError, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  protected http = inject(HttpClient);
+
+  get<T>(url: string): Observable<T> {
+    return this.http
+      .get<T>(API_BASE + url)
+      .pipe(catchError((error) => {
+        console.error('GET request error:', error);
+        throw error;
+      }));
+  }
+
+  // post<T>(url: string) {}
+
+  // put<T>(url: string) {}
+
+  // delete<T>(url: string) {}
+}
