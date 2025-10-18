8ec541f7fae9919728d6bd12d421600fcaf6d218
Thanasis <makris_th@hotmail.com>
2025-10-18 11:27:11 +0300

    photo service domain specific logic

--- src/app/core/services/photo.service.ts
+++ src/app/core/services/photo.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { API_BASE, API_ENDPOINTS } from '../constants/api.constant';
+import { mapApiResponseToPhotos } from './utils';
+import { map, Observable } from 'rxjs';
+import { BaseService } from './base.service';
+export interface Photo {
+  id: string;
+  src: string;
+  alt: string;
+  previewUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
-  
+  private baseService = inject(BaseService);
+
+  public getPhotos(page?: number, limit?: number): Observable<Photo[]> {
+    return this.baseService.get<Photo[]>(API_ENDPOINTS.PHOTO_LIST(page, limit)).pipe(
+      map(data => mapApiResponseToPhotos(data))
+    );
+  }
 }
