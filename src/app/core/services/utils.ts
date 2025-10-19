c1fa94c083380f9f77934608cbffd68587bf70a7
Thanasis <makris_th@hotmail.com>
2025-10-19 11:10:40 +0300

    utils opt

--- src/app/core/services/utils.ts
+++ src/app/core/services/utils.ts
@@ -1,4 +1,5 @@
-import { Photo } from "./photo.service";
+import { FAVORITES_KEY } from "./favorites.service";
+import { Photo, PHOTO_KEY } from "./photo.service";
 
 export function mapApiResponseToPhotos(data: any[]): Photo[] {
   return data.map((item: any) => ({
@@ -9,4 +10,25 @@ export function mapApiResponseToPhotos(data: any[]): Photo[] {
     height: item.height,
     author: item.author
   }));
+}
+
+export function mapApiResponseToPhoto(data: any): Photo {
+  return {
+    id: data.id,
+    src: data.download_url,
+    alt: data.url,
+    width: data.width,
+    height: data.height,
+    author: data.author
+  };
+}
+
+export function restoreFavoritePhotos(): Photo[] {
+  const stored = localStorage.getItem(FAVORITES_KEY);
+  return stored ? JSON.parse(stored) : [];
+}
+
+export function restoreDetailedPhoto(): Photo[] {
+  const stored = localStorage.getItem(PHOTO_KEY);
+  return stored ? JSON.parse(stored) : [];
 }
