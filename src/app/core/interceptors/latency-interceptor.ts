13c496990417980f9c9b5662848baae554a801f9
Thanasis <makris_th@hotmail.com>
2025-10-19 11:10:32 +0300

    interceptor

--- src/app/core/interceptors/latency-interceptor.ts
+++ src/app/core/interceptors/latency-interceptor.ts
@@ -2,9 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { delay } from 'rxjs';
 
 export const latencyInterceptor: HttpInterceptorFn = (req, next) => {
-  // only delay image-list API calls (not static assets)
-  const shouldDelay = req.url.includes('picsum.photos');
-  if (!shouldDelay) return next(req);
+  if (!req.url.includes('picsum.photos')) return next(req);
   const random = 200 + Math.floor(Math.random() * 100);
   return next(req).pipe(delay(random));
 };
