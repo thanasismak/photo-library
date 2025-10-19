cf984b494cacdbc6074e47a3cc73ef0886198711
Thanasis <makris_th@hotmail.com>
2025-10-19 17:10:28 +0300

    comments

--- src/app/core/interceptors/latency-interceptor.ts
+++ src/app/core/interceptors/latency-interceptor.ts
@@ -3,6 +3,7 @@ import { delay } from 'rxjs';
 
 export const latencyInterceptor: HttpInterceptorFn = (req, next) => {
   if (!req.url.includes('picsum.photos')) return next(req);
+  // Loading new photos should have a random delay of 200-300ms.
   const random = 200 + Math.floor(Math.random() * 100);
   return next(req).pipe(delay(random));
 };
